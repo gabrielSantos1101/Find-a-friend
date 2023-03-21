7c8f8a9689becada45bdb4331e2b78eaca4dd2e7
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-03-21 20:42:18 -0300

    feat: rebranding mobile layout

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let menuList = document.querySelector('.menu-list')
 inputMenu.addEventListener('change', () => {
   menuList.classList.toggle('hidden')
 })
+
+// if (window.screen.width <= 768) {
+//   menuList.classList.remove('hidden')
+// } else {
+//   menuList.classList.add('hidden')
+// }
