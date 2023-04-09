799a022f0f52a7e09868368f3e3162ac12d5ccd6
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-04-08 23:12:05 -0300

    fix: responsiveness contact card

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ let menuList = document.querySelector('.menu-list')
 const menuHamburger = document.querySelector('.hamburger-menu')
 
 inputMenu.addEventListener('change', () => {
-  menuList.classList.toggle('hidden')
+  if (window.innerWidth >= 780) {
+    menuList.classList.toggle('hidden')
+    menuHamburger.classList.add('hidden')
+  } else {
+    menuList.classList.add('hidden')
+    menuHamburger.classList.remove('hidden')
+  }
 })
 
 window.addEventListener('load', () => {
